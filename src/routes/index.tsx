bab54e25106a70e7a597ae7bb11c0092100dfad0
NeoTRAN001 <neo_tran@outlook.com>
2023-06-07 00:05:30 -0600

    feat: add Tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,11 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
-import Counter from '~/components/starter/counter/counter';
-import Hero from '~/components/starter/hero/hero';
-import Infobox from '~/components/starter/infobox/infobox';
-import Starter from '~/components/starter/next-steps/next-steps';
-
 export default component$(() => {
   return (
-    <><h1>Hola mundo</h1></>
+    <>
+      <span class="text-9xl">Hola mundo</span>
+    </>
   );
 });
 
